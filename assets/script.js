f87e7b70f6be794641a5979e33a46217b4623b2a
bashkos <bashkos@gmail.com>
2017-07-02 21:36:01 +0800

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+function initParallax (parallax) {
+  var background = parallax.querySelector('.parallax__background')
+  var time = Date.now()
+
+  var update = function (force) {
+    if (Date.now() - time >= 10 || force) {
+      time = Date.now()
+
+      var clientHeight = document.documentElement.clientHeight
+      var bounds = parallax.getBoundingClientRect()
+      if (bounds.top < clientHeight && bounds.bottom > 0) {
+        console.log('parallax')
+
+        var delta = Math.max((bounds.height - clientHeight) / 2, 0)
+        var offset = 0
+
+        if (bounds.top < -delta) {
+          offset = -bounds.top - delta
+        } else if (bounds.bottom > clientHeight + delta) {
+          offset = clientHeight + delta - bounds.bottom
+        }
+
+        window.requestAnimationFrame(function () {
+          background.style.transform = 'translateY(' + (offset * .7) + 'px)'
+        });
+      } else {
+        console.log('sleep')
+      }
+    }
+  }
+
+  window.addEventListener('scroll', update)
+  window.addEventListener('recise', update.bind(null, true))
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var parallaxSet = document.querySelectorAll('.parallax')
+  for (var i = 0; i < parallaxSet.length; i += 1) {
+    initParallax(parallaxSet[i])
+  }
+})
